34da8529de5f523b58e56bdc02b21b06ce0ebfc7
Roman Izegov <izegovrg@yandex.ru>
2024-03-11 15:45:54 +0300

    created community models

--- community/models.py
+++ community/models.py
@@ -1,3 +1,58 @@
 from django.db import models
+from community.constants import (MAX_CHAR_FIELD_LENGTH,
+                                 MAX_CHAR_FIELD_POSTCODE_LENGTH)
 
-# Create your models here.
+
+class Organisation(models.Model):
+    title = models.CharField(
+        max_length=MAX_CHAR_FIELD_LENGTH,
+        verbose_name='Название организации'
+    )
+    description = models.TextField(
+        verbose_name='Описание организации'
+    )
+    address = models.CharField(
+        max_length=MAX_CHAR_FIELD_LENGTH,
+        verbose_name='Адрес организации'
+    )
+    postcode = models.CharField(
+        max_length=MAX_CHAR_FIELD_POSTCODE_LENGTH,
+        verbose_name='Почтовый индекс'
+    )
+
+    class Meta:
+        verbose_name = 'Организация'
+        verbose_name_plural = 'Организации'
+
+    def __str__(self):
+        return self.title
+
+
+class Events(models.Model):
+    title = models.CharField(
+        max_length=MAX_CHAR_FIELD_LENGTH,
+        verbose_name='Название мероприятия'
+    )
+    description = models.TextField(
+        verbose_name='Описание мероприятия'
+    )
+    organizations = models.ManyToManyField(
+        Organisation,
+        verbose_name='Организаторы мероприятия'
+    )
+    image = models.ImageField(
+        upload_to='events/images/',
+        verbose_name='Фотография мероприятия',
+        blank=True,
+        null=True
+    )
+    date = models.DateField(
+        verbose_name='Дата проведения мероприятия'
+    )
+
+    class Meta:
+        verbose_name = 'Мероприятие'
+        verbose_name_plural = 'Мероприятия'
+
+    def __str__(self):
+        return self.title
